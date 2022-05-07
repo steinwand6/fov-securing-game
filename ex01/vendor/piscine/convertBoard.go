11f9e8ebbaeb11dac8935a7fef48a43462e4c59b
tyamagis <tyamagis@outlook.com>
2022-05-08 00:42:50 +0900

    ADD convert board [][]rune to [][]int

---
+++ ex01/vendor/piscine/convertBoard.go
@@ -0,0 +1,21 @@
+package piscine
+
+import "fmt"
+
+func convBoardRtoInt(b [][]rune) [][]int {
+	ib := make([][]int, len(b))
+	for i, s := range b {
+		ib[i] = make([]int, len(s))
+		for j, r := range s {
+			if IsNumeric(r) {
+				ib[i][j] = int(r - '0')
+			} else if r == '.' {
+				ib[i][j] = 0
+			} else {
+				ib[i][j] = int(r - 'a' + 10)
+			}
+		}
+	}
+	fmt.Println("intBoard : ", ib)
+	return ib
+}
